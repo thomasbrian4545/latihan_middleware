885416422894a78c7e745b9a825311ffdda9680e
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-23 23:36:58 +0700

    Membuat MahasiswaController dan ...
    - CobaMiddleware.php
    - Membuat route (3)
    - Mendaftarkan CobaMiddleware di Kernel.php

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
 {
-    //
+    public function daftarMahasiswa()
+    {
+        return 'Form pendaftaran mahasiswa';
+    }
+    public function tabelMahasiswa()
+    {
+        return 'Tabel data mahasiswa';
+    }
+    public function blogMahasiswa()
+    {
+        return 'Halaman blog mahasiswa';
+    }
 }
