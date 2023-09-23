384a92a19309b6671ddfdc7bbe975a08eacf56f8
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-23 23:43:08 +0700

    Mengaktifkan middleware dari Route

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\CobaMiddleware::class,
+
     ];
 
     /**
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
         ],
 
         'api' => [
@@ -65,5 +66,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'coba' => \App\Http\Middleware\CobaMiddleware::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
 
-Route::get('/daftar-mahasiswa', [MahasiswaController::class, 'daftarMahasiswa']);
+Route::get('/daftar-mahasiswa', [MahasiswaController::class, 'daftarMahasiswa'])->middleware('coba');
 Route::get('/tabel-mahasiswa', [MahasiswaController::class, 'tabelMahasiswa']);
-Route::get('/blog-mahasiswa', [MahasiswaController::class, 'blogMahasiswa']);
+Route::get('/blog-mahasiswa', [MahasiswaController::class, 'blogMahasiswa'])->middleware('coba');
